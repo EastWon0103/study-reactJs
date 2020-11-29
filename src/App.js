e97c87fc88dd97e6aab392f3f1b90e5e9e4a5d94
DongwonKim <dongwon0103@naver.com>
2020-11-29 17:09:59 +0900

    Dynamic Component Generation part

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
-function Food({fav}){
-  return <h2>I like {fav}</h2>;
+function Food({name, image}){
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={image} />
+  </div>
 }
+
+const foodILike = [
+  {
+    name: "kimchi",
+    image: "https://i.ytimg.com/vi/4wLo1tH_Ee8/maxresdefault.jpg"
+  },
+  {
+    name: "pizza",
+    image: 
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FP-Ojv_1veCg%2Fmaxresdefault.jpg&f=1&nofb=1"
+  },
+  {
+    name: "chicken",
+    image:
+    "https://www.tasteofhome.com/wp-content/uploads/2018/04/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS-1.jpg"
+  }
+];
 
 function App() {
   return (
   <div>
-    <h1>Hello</h1>
-    <Food fav="kimchi" />
-    <Food fav="ramen" />
-    <Food fav="potato" />
-    <Food fav="salad" />
-    <Food fav="pizza" />
-    <Food fav="chicken" />
+    {foodILike.map(dish => (
+      <Food name={dish.name} image={dish.image} />
+    ))}
   </div>
   );
 }
